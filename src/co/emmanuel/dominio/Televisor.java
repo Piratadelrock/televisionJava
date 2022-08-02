05175ed92fa156dfe80c7b0db63ac5274cc8f022
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2022-08-01 21:52:46 -0500

    televisor, caracteristicas

---
+++ src/co/emmanuel/dominio/Televisor.java
@@ -0,0 +1,10 @@
+package co.emmanuel.dominio;
+
+public class Televisor {
+    public short tamano;
+    public String marca;
+    public boolean encendido;
+    public short canalSintonizado;
+    public byte volumenActual;
+
+}
